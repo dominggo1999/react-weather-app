2f0885af7bad4f5c67278ac06bc3b1bbe166aa17
dominggo1999 <arnolddominggo19@gmail.com>
2020-06-03 02:18:15 +0700

    template

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React from 'react'
 import './App.scss'
-import { Provider } from 'react-redux'
-import store  from './redux/store.js'
-import CakeContainer from './components/CakeContainer.js'
-
 
 class App extends React.Component {
 	render() {
 		return (
-			<Provider store = { store }>
-				<div>
-					<CakeContainer />
-				</div>
-			</Provider>
+			<div>
+
+			</div>
 		)
 	}
 }
